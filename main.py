d0e24656506fda3faaf7e2af923a03f4ab9963f6
Rasmus Skovbo <rasmusskovbo.dk@gmail.com>
2021-10-26 21:10:09 +0200

    2.3

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print('hello world')
-print('edit')
+# Excercises
+## Chapter 2
+### 2.3
+def for_the_td(player1, player2):
+    return f'{player1} to {player2} for the TD!'
+
+print(for_the_td("Dak", "Zeke"))
+
