4f8da2f710a15253dddfb4e72e17385eea2e53cf
Rasmus Skovbo <rasmus.skovbo@shape.dk>
2021-10-26 21:02:23 +0200

    excercises

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print('hello world')
-print('edit')
+# FF
+## 2.5
+def is_leveon(player):
+    if player.lower() == "le'veon bell":
+        return True
+    else:
+        return False
+
+def commentary(score):
+    if score >= 100:
+        return f'{score} is a good score'
+    else:
+        return f'{score} is not a good score'
+
+positions = ['QB', 'RB', 'WR', 'TE', 'K', 'DST']
