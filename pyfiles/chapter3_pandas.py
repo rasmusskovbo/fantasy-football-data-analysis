1d1b6304d5c745552fca045449f2902c27f36121
Rasmus Skovbo <rasmus.skovbo@shape.dk>
2021-10-27 19:35:24 +0200

    Exc

--- pyfiles/chapter3_pandas.py
+++ pyfiles/chapter3_pandas.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 # C:\Users\rasmu\PycharmProjects\LTCW_Fantasy_Football\Ressources\ltcwff-files-0.9.0\data
 DATA_DIR = "C:/Users/rasmu/PycharmProjects/LTCW_Fantasy_Football/Ressources/ltcwff-files-0.9.0/data"
+DATA_DIR_MAC = "/Users/rasmusskovbo/PycharmProjects/fantasy-football-data-analysis/Ressources/ltcwff-files-0.9.0/data"
 
+adp_simple = pd.DataFrame(index = ['name', 'position', 'adp'])
